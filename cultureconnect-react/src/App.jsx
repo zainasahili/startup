2d56b5500218ce74f3cfaf4153977baa204327f8
zaina al-sahili <zsahili@byu.edu>
2025-10-06 22:58:44 -0600

    reorganized files so i can connect index.jsx to app.jsx

--- cultureconnect-react/src/App.jsx
+++ cultureconnect-react/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './app.css';
+import Login from "./pages/login";
+import Map from "./pages/map";
+import Quiz from "./pages/quiz";
+import Scoreboard from "./pages/scoreboard";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <div className="body bg-dark text-light">
+        <header className="navbar navbar-dark bg-dark px-3">
+        <h1 className="navbar-brand">CultureConnect</h1>
+        <nav>
+            <menu className="navbar-nav">
+                <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                    Home
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="quiz">
+                    Daily Quiz
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="scoreboard">
+                    ScoreBoard
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="login">
+                    Login / Register
+                </NavLink>
+                </li>
+            </menu>
+        </nav>
+        </header>
+
+        <Routes>
+                  <Route path='/login' element={<Login />} />
+                  <Route path='/quiz' element={<Quiz />} />
+                  <Route path='/scoreboard' element={<Scoreboard />} />
+                  <Route path='/' element={<Home />} />
+                  <Route path='/map' element={<Map />} />
+                  <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer className="bg-dark text-white-50">
+            <div className="container-fluid">
+            <span className="text-reset">My GitHub Repo</span>
+            <a className="text-reset" href="https://github.com/zainasahili/startup">
+                Source
+            </a>
+            </div>
+        </footer>
+    </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+}
+
+
