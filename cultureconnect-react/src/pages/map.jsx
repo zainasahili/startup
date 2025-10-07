4d5e448d35e16b9cc0bf4b12a70d42c6a8d8ac7a
zaina al-sahili <zsahili@byu.edu>
2025-10-07 13:03:18 -0600

    map page

--- cultureconnect-react/src/pages/map.jsx
+++ cultureconnect-react/src/pages/map.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export function Map() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>map displayed here</div>
+      <p>This page will show an interactive SVG world map. For now it's a placeholder image</p>
+      <img src="public/worldmap.png" alt="World map" className="center-image" />
+        <section>
+            <h2>Map features (planned)</h2>
+            <ul>
+                <li>Hover for quick facts</li>
+                <li>Click country to learn more</li>
+            </ul>
+        </section>
     </main>
   );
 }
