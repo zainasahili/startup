4df6afa03a15a1dc9535dbc52e075e7ad9963553
zaina al-sahili <zsahili@byu.edu>
2025-10-07 13:24:49 -0600

    daily quiz updated

--- cultureconnect-react/src/pages/quiz.jsx
+++ cultureconnect-react/src/pages/quiz.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 export function Quiz() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>quiz displayed here</div>
+       <p>The daily quiz will show here but for now it's a placeholder</p>
+        <form className = "quiz">
+            <fieldset>
+                <legend> Smaple question</legend>
+                <h3><strong>In Japan, which greeting is most common?</strong></h3>
+                <p><input type="radio" name="q1" value="a" /> Handshake</p>
+                <p><input type="radio" name="q1" value="b" /> Bow</p>
+                <p><input type="radio" name="q1" value="c" /> High-five</p>
+                <button type="submit" className="quiz_submit">Submit</button>
+            </fieldset>
+        </form>
+        <section id="result">Result will appear here</section>
     </main>
   );
 }
