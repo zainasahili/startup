abfc1807305f9714643af31abdbbc2ddb1636b93
zaina al-sahili <zsahili@byu.edu>
2025-10-06 22:28:13 -0600

    connecting index to app and creating initial routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './app.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'app.css';
+import Login from "./pages/login";
+import Map from "./pages/map";
+import Quiz from "./pages/quiz";
+import Scoreboard from "./pages/scoreboard";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/scoreboard" element={<Scoreboard />} />
+          <Route path="/about" element={<Login />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+export default App;
