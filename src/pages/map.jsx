eb258b197688165937b13f4f272fbbf8b5031e70
zaina al-sahili <zsahili@byu.edu>
2025-10-20 16:22:31 -0600

    shows country name when hovering or clicking

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -11,6 +11,11 @@ export function Map() {
   return (
     <main>
       <p>Hover over a country to see its name. Click functionality coming soon!</p>
+
+      <div style={{ textAlign: 'center', marginBottom: '10px', fontWeight: 'bold' }}>
+        {hoveredCountry && <span>{hoveredCountry}</span>}
+      </div>
+
       <ComposableMap>
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
@@ -19,16 +24,17 @@ export function Map() {
                 key={geo.rsmKey}
                 geography={geo}
                 onMouseEnter={() => {
-                  const { NAME } = geo.properties;
-                  setHoveredCountry(NAME);
+                  const countryName = geo.properties.name;
+                  setHoveredCountry(countryName);
                 }}
                 onMouseLeave={() => {
                   setHoveredCountry('');
                 }}
                 onClick={() => {
-                  alert(`You clicked on ${geo.properties.NAME}. API link coming soon!`);
+                  alert(`You clicked on ${geo.properties.name}. API link coming soon!`);
                 }}
                 style={{
+          
                   default: { fill: '#ac64b1', outline: '#6e3972' },
                   hover: { fill: '#fafec8', outline: 'none' },
                   pressed: { fill: '#6e3972', outline: 'none' },
