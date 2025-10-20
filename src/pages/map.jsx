427db2fadc7ea7ab9b79eb9fce1a2a149da1e608
zaina al-sahili <zsahili@byu.edu>
2025-10-20 15:50:52 -0600

    added initial interactive map

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ComposableMap, Geographies, Geography} from 'react-simple-maps';
+
+const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+
 
 export function Map() {
+
+  const [hoveredCountry, setHoveredCountry] = useState('');
+
   return (
     <main>
-      <p>This page will show an interactive SVG world map. For now it's a placeholder image</p>
-      <img src="/worldmap.png" alt="World map" className="center-image" />
-        <section>
-            <h2>Map features (planned)</h2>
-            <ul>
-                <li>Hover for quick facts</li>
-                <li>Click country to learn more</li>
-            </ul>
-        </section>
+      <p>Hover over a country to see its name. Click functionality coming soon!</p>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onMouseEnter={() => {
+                  const { NAME } = geo.properties;
+                  setHoveredCountry(NAME);
+                }}
+                onMouseLeave={() => {
+                  setHoveredCountry('');
+                }}
+                onClick={() => {
+                  alert(`You clicked on ${geo.properties.NAME}. API link coming soon!`);
+                }}
+                style={{
+                  default: { fill: '#ac64b1', outline: '#6e3972' },
+                  hover: { fill: '#fafec8', outline: 'none' },
+                  pressed: { fill: '#6e3972', outline: 'none' },
+                }}
+              />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
     </main>
   );
 }
